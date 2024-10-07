311593794c8a7e8e20c83a643cc0bdab9df8c463
Zakary <chauve.zakary@gmail.com>
2024-10-07 23:45:13 +0200

    Check list added

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,3 +1,5 @@
+import { check } from "../assets";
+import { collabApps, collabContent } from "../constants";
 import Section from "./Section";
 
 const Collaboration = () => {
@@ -8,6 +10,19 @@ const Collaboration = () => {
           <h2 className="h2 mb-4 md:mb-8">
             AI Chat App for seamless collaboration
           </h2>
+          <ul className="max-w-[22rem] mb-10 md:mb-14">
+            {collabContent.map((item) => (
+              <li key={item.id} className="mb-3 py-3">
+                <div className="flex items-center">
+                  <img src={check} width={24} height={24} alt="ckeck" />
+                  <h6 className="body-2 ml-5">{item.title}</h6>
+                </div>
+                {item.text && (
+                  <p className="body-2 mt-3 text-n-4">{item.text}</p>
+                )}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </Section>
