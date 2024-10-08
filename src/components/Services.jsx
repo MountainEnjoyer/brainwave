9ba7e3d2c9d5f3bb5417f8fe24f8a2fc0cd9c3b2
Zakary <chauve.zakary@gmail.com>
2024-10-08 09:15:48 +0200

    One card done

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,49 @@
+import { service1, service2, service3, check } from "../assets";
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
+import Heading from "./Heading";
+import Section from "./Section";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <Section id="how-to-use">
+      <div className="container">
+        <Heading
+          title="Generative AI made for creators."
+          text="Brainwave unlocks the potential of AI-Powered applications"
+        />
+        <div className="relative">
+          <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:-p20 xl:h-[46rem]">
+            <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 wl-w-auto">
+              <img
+                className="w-full h-full object-cover md:object-right"
+                width={800}
+                height={730}
+                src={service1}
+                alt="smartest AI"
+              />
+            </div>
+            <div className="relative z-1 max-w-[17rem] ml-auto">
+              <h4 className="h4 mb-4">Smartest AI</h4>
+              <p className="body-2 mb-[3rem] text-n-3">
+                Brainwave unlocks the potential of AI-Powered applications
+              </p>
+              <ul className="body-2">
+                {brainwaveServices.map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex items-start py-4 border-t border-n-6"
+                  >
+                    <img src={check} />
+                    <p className="ml-4">{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default Services;
